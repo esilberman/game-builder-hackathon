848ae2fff29f7072273c461d64e66a902bf4ad5a
esilberman <eve.silberman7390@gmail.com>
2025-02-23 20:17:26 -0500

    size

--- src/components/Art.tsx
+++ src/components/Art.tsx
@@ -211,7 +211,7 @@ export const Art = ({  }: ArtProps) => {
         req: {
             text: input,
             image: imgUrl,
-            img_size: 400,
+            img_size: 512,
         },
     }).then(response => {
         setAiImage(response.url);
